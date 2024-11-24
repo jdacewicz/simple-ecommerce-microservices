9bedf0642f06eca99b56fec55fc22a89180c7226
Jakub Dacewicz <jakub.dacewicz99@gmail.com>
2024-11-24 18:05:16 +0100

    Created cart item collection

---
+++ cart-service/src/main/java/dev/jakubdacewicz/cart_service/cart/CartItem.java
@@ -0,0 +1,118 @@
+package dev.jakubdacewicz.cart_service.cart;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Document(collection = "cart_items")
+public class CartItem {
+
+    @Id
+    private String id;
+
+    private String cartId;
+
+    private String productId;
+
+    private int quantity;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Indexed(unique = true)
+    private UUID businessKey;
+
+    public CartItem(String id,
+                    String cartId,
+                    String productId,
+                    int quantity,
+                    LocalDateTime createdAt,
+                    LocalDateTime updatedAt,
+                    UUID businessKey) {
+        this.id = id;
+        this.cartId = cartId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.businessKey = businessKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return Objects.equals(businessKey, cartItem.businessKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(businessKey);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public UUID getBusinessKey() {
+        return businessKey;
+    }
+
+    void setId(String id) {
+        this.id = id;
+    }
+
+    void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    void setBusinessKey(UUID businessKey) {
+        this.businessKey = businessKey;
+    }
+}
