f3bda4f34f974d27f09ffde0859f3c95d90574cb
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2024-11-24 19:22:12 +0100

    Revert "feat: cart builders"

--- cart-service/src/main/java/dev/jakubdacewicz/cart_service/config/mongodb/MongoDBConfiguration.java
+++
@@ -1,9 +0,0 @@
-package dev.jakubdacewicz.cart_service.config.mongodb;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@EnableMongoAuditing
-@Configuration
-public class MongoDBConfiguration {
-}
