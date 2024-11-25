7f6df8105093ee53438a3d2a2cb2418d3d955183
Jakub Dacewicz <jakub.dacewicz99@gmail.com>
2024-11-25 15:35:14 +0100

    Validated CategoryRestControllerV1

--- product-service/src/main/java/dev/jakubdacewicz/product_service/category/controller/CategoryRestControllerV1.java
+++ product-service/src/main/java/dev/jakubdacewicz/product_service/category/controller/CategoryRestControllerV1.java
@@ -2,10 +2,16 @@ package dev.jakubdacewicz.product_service.category.controller;
 
 import dev.jakubdacewicz.product_service.category.CategoryService;
 import dev.jakubdacewicz.product_service.category.dto.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/products/categories")
 public class CategoryRestControllerV1 {
 
@@ -16,41 +22,41 @@ public class CategoryRestControllerV1 {
     }
 
     @GetMapping("/{id}")
-    public SummaryCategoryDto getCategory(@PathVariable String id) {
+    public SummaryCategoryDto getCategory(@NotBlank @PathVariable String id) {
         return categoryService.getCategory(id);
     }
 
     @GetMapping("/{id}/details")
-    public DetailedCategoryDto getCategoryDetails(@PathVariable String id) {
+    public DetailedCategoryDto getCategoryDetails(@NotBlank @PathVariable String id) {
         throw new UnsupportedOperationException();
     }
 
     @GetMapping
-    public Page<SummaryCategoryDto> getCategories(@RequestParam(defaultValue = "0") int page,
-                                                  @RequestParam(defaultValue = "10") int size,
+    public Page<SummaryCategoryDto> getCategories(@PositiveOrZero @RequestParam(defaultValue = "0") int page,
+                                                  @Positive @RequestParam(defaultValue = "10") int size,
                                                   @RequestParam(required = false) String name) {
         throw new UnsupportedOperationException();
     }
 
     @PostMapping
-    public SummaryCategoryDto createCategory(@RequestBody CategoryCreationRequest request) {
+    public SummaryCategoryDto createCategory(@Valid @RequestBody CategoryCreationRequest request) {
         return categoryService.createCategory(request);
     }
 
     @PutMapping("/{id}")
-    public CategoryUpdateResult updateCategory(@PathVariable String id,
-                                               @RequestBody CategoryUpdateRequest request) {
+    public CategoryUpdateResult updateCategory(@NotBlank @PathVariable String id,
+                                               @Valid @RequestBody CategoryUpdateRequest request) {
         throw new UnsupportedOperationException();
     }
 
     @PutMapping("/{id}/enable")
-    public CategoryEnableUpdateResult updateCategoryEnable(@PathVariable String id,
+    public CategoryEnableUpdateResult updateCategoryEnable(@NotBlank @PathVariable String id,
                                                            @RequestParam boolean enabled) {
         throw new UnsupportedOperationException();
     }
 
     @DeleteMapping("/{id}")
-    public CategoryDeletionResult deleteCategory(@PathVariable String id) {
+    public CategoryDeletionResult deleteCategory(@NotBlank @PathVariable String id) {
         throw new UnsupportedOperationException();
     }
 }
---
+++ product-service/src/main/java/dev/jakubdacewicz/product_service/shared/advice/GlobalRestControllerAdvice.java
@@ -0,0 +1,44 @@
+package dev.jakubdacewicz.product_service.shared.advice;
+
+import dev.jakubdacewicz.product_service.shared.exception.DocumentNotFoundException;
+import dev.jakubdacewicz.product_service.shared.model.ApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalRestControllerAdvice {
+
+    @ExceptionHandler(DocumentNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleDocumentNotFoundException(DocumentNotFoundException exception) {
+        return new ApiError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        String detailedMessage = exception.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ))
+                .entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + ": " + String.join(", ", entry.getValue()))
+                .collect(Collectors.joining("; "));
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), detailedMessage);
+    }
+}
