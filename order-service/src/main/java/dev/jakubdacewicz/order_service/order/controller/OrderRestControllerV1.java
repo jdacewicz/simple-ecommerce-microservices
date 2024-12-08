ebf350161641d0cd49728fe119b7cd803852c35f
Jakub Dacewicz <jakub.dacewicz99@gmail.com>
2024-12-08 17:44:01 +0100

    Validated orders api

--- order-service/src/main/java/dev/jakubdacewicz/order_service/order/controller/OrderRestControllerV1.java
+++ order-service/src/main/java/dev/jakubdacewicz/order_service/order/controller/OrderRestControllerV1.java
@@ -5,37 +5,44 @@ import dev.jakubdacewicz.order_service.order.dto.OrderCreationRequest;
 import dev.jakubdacewicz.order_service.order.dto.OrderStatusUpdateResult;
 import dev.jakubdacewicz.order_service.order.dto.SummaryOrderDto;
 import dev.jakubdacewicz.order_service.shared.types.OrderStatus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/orders")
 public class OrderRestControllerV1 {
 
     @GetMapping("/{id}")
-    public SummaryOrderDto getOrder(@PathVariable String id) {
+    public SummaryOrderDto getOrder(@NotBlank @PathVariable String id) {
         throw new UnsupportedOperationException();
     }
 
     @GetMapping("/{id}/details")
-    public DetailedOrderDto getOrderDetails(@PathVariable String id) {
+    public DetailedOrderDto getOrderDetails(@NotBlank @PathVariable String id) {
         throw new UnsupportedOperationException();
     }
 
     @GetMapping
-    public Page<SummaryOrderDto> getOrders(@RequestParam(defaultValue = "0") int page,
-                                           @RequestParam(defaultValue = "10") int size) {
+    public Page<SummaryOrderDto> getOrders(@PositiveOrZero @RequestParam(defaultValue = "0") int page,
+                                           @Positive @RequestParam(defaultValue = "10") int size) {
         throw new UnsupportedOperationException();
     }
 
     @PostMapping
-    public DetailedOrderDto createOrder(@RequestBody OrderCreationRequest request) {
+    public DetailedOrderDto createOrder(@Valid @RequestBody OrderCreationRequest request) {
         throw new UnsupportedOperationException();
     }
 
     @PutMapping("/{id}/status/{status}")
-    public OrderStatusUpdateResult updateOrderStatus(@PathVariable String id,
-                                                     @PathVariable OrderStatus status) {
+    public OrderStatusUpdateResult updateOrderStatus(@NotBlank @PathVariable String id,
+                                                     @NotNull @PathVariable OrderStatus status) {
         throw new UnsupportedOperationException();
     }
 }
