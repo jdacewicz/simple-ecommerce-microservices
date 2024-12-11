822ad404c7bfdb2732f72ac639b431978ae7283c
Jakub Dacewicz <jakub.dacewicz99@gmail.com>
2024-12-11 10:30:08 +0100

    Connected redis to cart service

---
+++ cart-service/src/main/java/dev/jakubdacewicz/cart_service/config/redis/RedisConfiguration.java
@@ -0,0 +1,18 @@
+package dev.jakubdacewicz.cart_service.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        return template;
+    }
+}
