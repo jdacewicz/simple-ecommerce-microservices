d4f31347f807ed3f259031dfd7ec628541a6ff75
Jakub Dacewicz <jakub.dacewicz99@gmail.com>
2024-12-13 18:04:50 +0100

    Could not find FindByIndexNameSessionRepository bean - fix

--- cart-service/src/main/java/dev/jakubdacewicz/cart_service/config/redis/RedisConfiguration.java
+++ cart-service/src/main/java/dev/jakubdacewicz/cart_service/config/redis/RedisConfiguration.java
@@ -1,20 +1,37 @@
 package dev.jakubdacewicz.cart_service.config.redis;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 2700)
 public class RedisConfiguration {
 
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<?, ?> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+    private final RedisConnectionFactory redisConnectionFactory;
 
-        return template;
+    RedisConfiguration(ObjectProvider<RedisConnectionFactory> redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory.getIfAvailable();
+    }
+
+    @Bean
+    public RedisOperations<String, Object> sessionRedisOperations() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(this.redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public FindByIndexNameSessionRepository<?> redisSessionRepository(RedisOperations<String, Object> sessionRedisOperations) {
+        return new RedisIndexedSessionRepository(sessionRedisOperations);
     }
 }
